bed2fc5d588ef86560ca8e6a35619d402af3cf15
Harsh <contact.stoxd@gmail.com>
2026-06-19 01:27:25 +0530

    Create calculator.py

---
+++ Basics/calculator.py
@@ -0,0 +1,17 @@
+print("=== Calculator ===")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operator = input("select operator: +, -, *, /")
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
