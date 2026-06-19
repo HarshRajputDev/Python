1fd92c009930bfec49f8c98b06192f632eab536d
Harsh <contact.stoxd@gmail.com>
2026-06-19 13:40:02 +0530

    Update fahrenheit_to_celsius.py

--- Basics/PracticeSet1/fahrenheit_to_celsius.py
+++ Basics/PracticeSet1/fahrenheit_to_celsius.py
@@ -4,4 +4,11 @@ print(start_text)
 fahrenheit = float(input("Enter temperature in fahrenheit: "))
 fah_to_cel_1 = (fahrenheit - 32)
 fah_to_cel_2 = (fah_to_cel_1 // 1.8)
-print(f'Fahrenheit to Celsius:\n {fah_to_cel_2}')
+print(f'Fahrenheit to Celsius:\n {fah_to_cel_2}°C')
+
+# Or this
+start_text = "This script converts temperature from fahrenheit to celsius. (it gives round off value)\n\n"
+print(start_text)
+fahrenheit = float(input("Enter temperature in fahrenheit: "))
+fah_to_cel = round((fahrenheit - 32) / 1.8)
+print(f'Fahrenheit to Celsius:\n {fah_to_cel}')
