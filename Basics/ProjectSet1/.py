67489629ff8581983b20481491240c8b27654d05
Harsh <contact.stoxd@gmail.com>
2026-06-19 12:23:10 +0530

    Add initial Python script with prints and self-message

---
+++ Basics/ProjectSet1/.py
@@ -0,0 +1,15 @@
+print("      1")
+print("    2 3")
+print("  4 5 6")
+print("7 8 9 10")
+
+print("H      H   K     K")
+print("H      H   K    K ")
+print("H      H   K  K   ")
+print("HHHHHHHH   KK     ")
+print("H      H   K  K   ")
+print("H      H   K    K ")
+print("H      H   K     K")
+
+# msg to future self
+print("Date: 19 june 2026\n\nexited to learn python. want to become a skilled programmer, Build useful projects, and solve problems with codes.\n\n Message to my future self:\nHope uh never stopped learning and keep improving every day, keep building amazing projects and believe in yourself!\n\n - @BrandedPsycho")
