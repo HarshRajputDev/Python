19844cab96dce34037376809d50ae18d9905759a
Harsh <contact.stoxd@gmail.com>
2026-06-21 23:22:01 +0530

    Update #2- variables & data types.py

--- Basics/#2- variables & data types.py
+++ Basics/#2- variables & data types.py
@@ -9,10 +9,25 @@ print(a+c)
 
 # Data types
 integers = 12, 8, -10
+print(integers)
+
 floats = 3.14, -0.01, 89
+print(floats)
+
 strings = "Hello", "Hi"
+print(strings)
+
 boolean = True, False
+print(boolean)
+
 lists = [1,2,3], ["a","b","c"]
+print(lists)
+
 tuples = (1,2,2), ("a","b","c")
+print(tuples)
+
 sets = {1,2,3}, {"a","b"}
+print(sets)
+
 dictionary = {"Name": "Harsh", "age": 5, "gender": "male"}
+print(dictionary)
